330a3120bb7d88cb5b38afefd58ad84c45823fc6
Renato <renatovp@ime.usp.br>
2020-07-20 21:17:41 -0300

    test handlers, prior_specs, and run expctd errors +outputs using testhat

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -2,59 +2,536 @@ library(bsem)
 library(magrittr)
 library(testthat)
 
+## Models (sem, print.bsem, summary.bsem, plot.bsem)
+dt <- simdata(Nna = 1)
+
 ## factorial
-dt <- simdata(exogenous = NULL, paths = NULL)
-fit <- bsem::sem(data = dt$data, blocks = dt$blocks, signals = dt$signals, iter = 100, chains = 1)
-fit
+fit <- bsem::sem(
+  data = dt$data %>% na.omit(),
+  blocks = dt$blocks,
+  signals = dt$signals,
+  iter = 2,
+  warmup = 0,
+  chains = 1
+) %>%
+  expect_s3_class(class = "bsem")
+
+fit$model == "factorial"
+
+print.bsem(fit)
 fit %>% summary()
 fit %>% plot()
+fit$mean_alpha %>% arrayplot()
 
 ### factorialNA
-dt <- simdata(exogenous = NULL, paths = NULL, Nna = 1)
-fit <- sem(data = dt$data, blocks = dt$blocks, signals = dt$signals, iter = 100, chains = 1)
-fit
+fit <- sem(
+  data = dt$data,
+  blocks = dt$blocks,
+  signals = dt$signals,
+  iter = 2,
+  warmup = 0,
+  chains = 1
+) %>%
+  expect_s3_class(class = "bsem")
+
+fit$model == "factorialNA"
+
+print.bsem(fit)
 fit %>% summary()
 fit %>% plot()
+fit$mean_alpha %>% arrayplot()
 
 ## factorialEX
-dt <- simdata(paths = NULL)
-fit <- sem(data = dt$data, blocks = dt$blocks, exogenous = dt$exogenous, signals = dt$signals, iter = 100, chains = 1)
-fit
+fit <- sem(
+  data = dt$data %>% na.omit(),
+  blocks = dt$blocks,
+  exogenous = dt$exogenous,
+  signals = dt$signals,
+  iter = 2,
+  warmup = 0,
+  chains = 1
+) %>%
+  expect_s3_class(class = "bsem")
+
+fit$model == "factorialEX"
+
+print.bsem(fit)
 fit %>% summary()
 fit %>% plot()
+fit$mean_alpha %>% arrayplot()
 
 ### factorialNAEX
-dt <- simdata(paths = NULL, Nna = 1)
-fit <- sem(data = dt$data, blocks = dt$blocks, exogenous = dt$exogenous, signals = dt$signals, iter = 100, chains = 1)
-fit
+fit <- sem(
+  data = dt$data,
+  blocks = dt$blocks,
+  exogenous = dt$exogenous,
+  signals = dt$signals,
+  iter = 2,
+  warmup = 0,
+  chains = 1
+) %>%
+  expect_s3_class(class = "bsem")
+
+fit$model == "factorialNAEX"
+
+print.bsem(fit)
 fit %>% summary()
 fit %>% plot()
+fit$mean_alpha %>% arrayplot()
 
 ## sem
-dt <- simdata(exogenous = NULL)
-fit <- sem(data = dt$data, blocks = dt$blocks, paths = dt$paths, signals = dt$signals, iter = 100, chains = 1)
-fit
+fit <- sem(
+  data = dt$data %>% na.omit(),
+  blocks = dt$blocks,
+  paths = dt$paths,
+  signals = dt$signals,
+  iter = 2,
+  warmup = 0,
+  chains = 1
+) %>%
+  expect_s3_class(class = "bsem")
+
+fit$model == "sem"
+
+print.bsem(fit)
 fit %>% summary()
 fit %>% plot()
+fit$mean_alpha %>% arrayplot()
 
 ### semNA
-dt <- simdata(exogenous = NULL, Nna = 1)
-fit <- sem(data = dt$data, blocks = dt$blocks, paths = dt$paths, signals = dt$signals, iter = 100, chains = 1)
-fit
+fit <- sem(
+  data = dt$data,
+  blocks = dt$blocks,
+  paths = dt$paths,
+  signals = dt$signals,
+  iter = 2,
+  warmup = 0,
+  chains = 1
+) %>%
+  expect_s3_class(class = "bsem")
+
+fit$model == "semNA"
+
+print.bsem(fit)
 fit %>% summary()
 fit %>% plot()
+fit$mean_alpha %>% arrayplot()
 
 ## semEX
-dt <- simdata()
-fit <- sem(data = dt$data, blocks = dt$blocks, paths = dt$paths, exogenous = dt$exogenous, signals = dt$signals, iter = 100, chains = 1)
-fit
+fit <- sem(
+  data = dt$data %>% na.omit(),
+  blocks = dt$blocks,
+  paths = dt$paths,
+  exogenous = dt$exogenous,
+  signals = dt$signals,
+  iter = 2,
+  warmup = 0,
+  chains = 1
+) %>%
+  expect_s3_class(class = "bsem")
+
+fit$model == "semEX"
+
+print.bsem(fit)
 fit %>% summary()
 fit %>% plot()
+fit$mean_alpha %>% arrayplot()
 
 ### semNAEX
-dt <- simdata(Nna = 1)
-fit <- sem(data = dt$data, blocks = dt$blocks, paths = dt$paths, exogenous = dt$exogenous, signals = dt$signals, iter = 100, chains = 1)
-fit
+fit <- sem(
+  data = dt$data,
+  blocks = dt$blocks,
+  paths = dt$paths,
+  exogenous = dt$exogenous,
+  signals = dt$signals,
+  iter = 2,
+  warmup = 0,
+  chains = 1
+) %>%
+  expect_s3_class(class = "bsem")
+
+fit$model == "semNAEX"
+
+print.bsem(fit)
 fit %>% summary()
 fit %>% plot()
+fit$mean_alpha %>% arrayplot()
 
+### handlers.R
+
+## Prior options
+expect_warning(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(
+      beta = "cauchy(0,1)",
+      sigma2 = "lognormal(-1,2)",
+      gamma = "cauchy(0,1)",
+      tau2 = "lognormal(-1,2)",
+      gamma0 = "cauchy(0,1)"
+    )
+  )
+) %>% expect_s3_class(class = "bsem")
+
+expect_warning(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(
+      beta = "normal(-2,1)",
+      sigma2 = "gamma(.1,.1)",
+      gamma = "cauchy(0,1)",
+      tau2 = "inv_gamma(1.1,2.2)",
+      gamma0 = "cauchy(0,1)"
+    )
+  )
+) %>% expect_s3_class(class = "bsem")
+
+expect_warning(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(
+      beta = "normal(-2,1)",
+      sigma2 = "inv_gamma(1.1,2.1)",
+      gamma = "cauchy(0,1)",
+      tau2 = "gamma(1.1,2.2)",
+      gamma0 = "cauchy(0,1)"
+    )
+  )
+) %>% expect_s3_class(class = "bsem")
+
+
+## Prior misspecification
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(beta = "gamma(0,1)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(gamma = "gamma(0,1)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(gamma0 = "gamma(0,1)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(sigma2 = "normal(.1,.1)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(tau2 = "normal(0,1)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(beta = "gamma")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(taual = "gamma0(0,1)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(gamma = "gamma(0.1,.1)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(gamma = "lognormal(0,2)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(lognsdçkj = "normal(0,2)")
+  )
+)
+
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(beta = "lognsdçkj(0,2)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(gamma = "lognsdçkj(0,2)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(gamma0 = "lognsdçkj(0,2)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(sigma2 = "lognsdçkj(0,2)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(tau2 = "lognsdçkj(0,2)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(gamma = "normal(-1,-2)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(gamma0 = "normal(-1,-2)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(beta = "normal(-1,-2)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(tau2 = "inv_gamma(-1,1.2)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(tau2 = "inv_gamma(1,-1.2)")
+  )
+)
+
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(sigma2 = "inv_gamma(-1,1.2)")
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    prior_specs = list(sigma2 = "inv_gamma(1,-1.2)")
+  )
+)
+
+## Stanformals
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    iter = 2,
+    warmup = 0,
+    chains = 1,
+    cilkdlçskjs = 1
+  )
+)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    signals = dt$signals[-1],
+    iter = 2,
+    warmup = 0,
+    chains = 1
+  )
+)
+
+aux <- dt$signals
+aux[[3]] <- 1
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    signals = aux,
+    iter = 2,
+    warmup = 0,
+    chains = 1
+  )
+)
+rm(aux)
+
+aux <- dt$paths
+names(aux)[2] <- "F6"
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    paths = aux,
+    signals = dt$signals,
+    iter = 2,
+    warmup = 0,
+    chains = 1
+  )
+)
+rm(aux)
+
+aux <- dt$paths
+aux[[1]][1] <- 3.5
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    paths = aux,
+    signals = dt$signals,
+    iter = 2,
+    warmup = 0,
+    chains = 1
+  )
+)
+rm(aux)
+
+expect_error(
+  sem(
+    data = dt$data,
+    blocks = dt$blocks,
+    paths = c(1),
+    signals = dt$signals,
+    iter = 2,
+    warmup = 0,
+    chains = 1
+  )
+)
